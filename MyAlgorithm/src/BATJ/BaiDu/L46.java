022ab5cdefa1f2fa260ec138824aedb1c7e9b8d5
jacklizekun <beike88888888@163.com>
2020-08-27 17:06:40 +0800

    1、L339 2、L349 3、L287 4、L268 5、L191 6、L160 7、L146 8、L141 9、L103 10、L72 11、L53 12、L29 13、L21 14、L20 15、L19 16、L15 17、Joffer51 18、Joffer42 19、Joffer39 20、Joffer21

--- MyAlgorithm/src/BATJ/BaiDu/L46.java
+++ MyAlgorithm/src/BATJ/BaiDu/L46.java
@@ -1,9 +1,32 @@
 package BATJ.BaiDu;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class L46 {
 
-	public L46() {
-		// TODO Auto-generated constructor stub
-	}
+	  public List<List<Integer>> permute(int[] nums) {
+		  List<List<Integer>> res = new LinkedList();
+		  ArrayList<Integer> output = new ArrayList<Integer>();
+		  for (int num : nums ) {
+			output.add(num);
+		}
+		  int n = nums.length;
+		  backtrack(n , output ,  res , 0);
+		  return res;
+	  }
+	  
+	 public void backtrack(int n,ArrayList<Integer> output, List<List<Integer>> res,int first) {
+		 if (first == n) {
+			res.add(new ArrayList<Integer>(output));
+		}
+		 for (int i = first; i < n; i++) {
+			Collections.swap(output, first, i);
+			backtrack(n, output, res, first + 1);
+			Collections.swap(output, first, i);
+		}
+	 }
 
 }
